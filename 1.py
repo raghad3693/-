cb224d20ab0bf26723f479efe8964008fe7097a1
raghad3693 <105462865+raghad3693@users.noreply.github.com>
2022-05-14 19:10:06 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+stu=["remana", "fahd", "nour" , "somer" , "bana"]
+n=input("enter the name:")
+if n in stu:
+    print("the student is graduat")
+else:
+    print("the student is not graduat")
+    
